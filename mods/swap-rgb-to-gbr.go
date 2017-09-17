62aefd6580d35552d0e83f179a01b9f033efa6d8
simran91 <simran@dn.gs>
2017-09-17 23:15:21 +1000

    added an explanation for why the things are the way they are... :)

--- mods/swap-rgb-to-gbr.go
+++ mods/swap-rgb-to-gbr.go
@@ -6,6 +6,15 @@ import "../monkey"
 // SwapRGBtoGBR is a mod that swaps the colours around... it's a very simple mod designed
 // to show how we loop over the ImageMatrix and read the colour values...
 //
+// IMPORTANT NOTE (or you will send yourself crazy wondering why this function doesn't actually
+// swap the colours around)....
+// eg. Doing R,G,B = G,B,R as we do below, copies the values (brightness) of the colours into the new
+//     fields... but the image when rendered is still interpreted as RGB...
+//     Consider the following for example:
+//       If a pixel in the original image was: R=255 G=10 B=20
+//       Then in the new image it will be    : R=10  G=20 B=255 (**remember** the image when rendered
+//       is always intepreted as RGB)
+//
 func SwapRGBtoGBR(matrix monkey.ImageMatrix) monkey.ImageMatrix {
 
 	width := matrix.GetWidth()
